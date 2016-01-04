0f93ae56f74294f194c36bab377d2a1b70644e4d
Cedric Lecomte <clecomte@redhat.com>
2016-01-04 16:42:36 -0500

    Adding the rspec capability through ansible_spec
    Change-Id: Idad9c8fb9ce3005b7349b7c2be276f04be656b45

---
+++ ansible/roles/common/spec/common_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe file('/etc/selinux/config') do
+  its(:content) { should match /^SELINUX=permissive$/ }
+end
+
+describe file('/etc/yum.repos.d/epel.repo') do
+  it { should exist }
+end
+
+describe package('dci-sshpubkeys') do
+  it { should be_installed }
+end
+
+describe file('/etc/sudoers.d/dci-user') do
+  it { should contain 'dci ALL=(ALL) NOPASSWD:ALL' }
+end
