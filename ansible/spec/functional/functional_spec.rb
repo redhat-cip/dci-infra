aa9ad169f354ad827b39712069b0b4ca18c2b52b
Yanis Guenane <yguenane@redhat.com>
2016-01-26 20:57:45 +0100

    functional testing: move wrapper from sh to python
    Initially the wrapper helper script was written in shell.
    Parsing output in shell can quickly become messy, since our client can
    return json, it is better readability wise to use a language like python
    than can parse json strings cleanly.
    
    Change-Id: I4f3f384c2f567c2668b47d47b25152c47735340d

--- ansible/spec/functional/functional_spec.rb
+++ ansible/spec/functional/functional_spec.rb
@@ -6,39 +6,39 @@ end
 
 # Team management
 #
-describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should contain('admin') }
   its(:stdout) { should_not match("/|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+describe command("python wrapper.py team-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
   its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-update #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_account_updated']}") do
+describe command("python wrapper.py team-update #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_account_updated']}") do
   its(:stdout) { should match("/|\s*message\s*|\s*Team updated.\s*|/") }
   its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account_updated']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should match("/|\s*#{property['test_account_updated']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account_updated']}") do
+describe command("python wrapper.py team-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account_updated']}") do
   its(:stdout) { should match("/|\s*message\s*|\s*Team deleted.\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should_not match("/|\s*#{property['test_account_updated']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
@@ -46,43 +46,43 @@ end
 
 # User Management
 #
-describe command("./wrapper.sh team-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+describe command("python wrapper.py team-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
   its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should contain('admin') }
   its(:stdout) { should_not match("/|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh user-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_password']}") do
+describe command("python wrapper.py user-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_password']}") do
   its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh user-update #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_account_updated']}") do
+describe command("python wrapper.py user-update #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_account_updated']}") do
   its(:stdout) { should match("/|\s*message\s*|\s*User updated.\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should match("/|\s*#{property['test_account_updated']}\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh user-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account_updated']}") do
+describe command("python wrapper.py user-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account_updated']}") do
   its(:stdout) { should match("/|\s*message\s*|\s*User deleted.\s*|/") }
   its(:exit_status) { should eq 0 }
 end
 
-describe command("./wrapper.sh team-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+describe command("python wrapper.py team-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
   its(:stdout) { should match("/|\s*message\s*|\s*Team deleted.\s*|/") }
   its(:exit_status) { should eq 0 }
 end
