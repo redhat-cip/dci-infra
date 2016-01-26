b156a6674dd45f9e3e95a9788675daeb9d24e389
Yanis Guenane <yguenane@redhat.com>
2016-01-26 20:57:45 +0100

    functional testing: move wrapper from sh to python
    Initially the wrapper helper script was written in shell.
    Parsing output in shell can quickly become messy, since our client can
    return json, it is better readability wise to use a language like python
    than can parse json strings cleanly.
    
    Change-Id: I4f3f384c2f567c2668b47d47b25152c47735340d

---
+++ ansible/wrapper.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import json
+import string
+import sys
+import subprocess
+
+def parse():
+    if len(sys.argv) < 4:
+        sys.exit('Usage: wrapper.py action login password cs-url [OPTIONS]')
+    ctx = {}
+    sys.argv.pop(0)
+    ctx['action'] = sys.argv.pop(0)
+    ctx['login'] = sys.argv.pop(0)
+    ctx['password'] = sys.argv.pop(0)
+    ctx['url'] = sys.argv.pop(0)
+    ctx['parameters'] = sys.argv
+
+    return ctx
+
+def user_management(ctx):
+    base_command = ['dcictl', '--dci-login', ctx['login'], '--dci-password', ctx['password'], '--dci-cs-url', ctx['url']]
+
+    if 'list' in ctx['action']:
+        command = base_command + [ctx['action']]
+    elif 'create' in ctx['action']:
+        command = base_command + ['--format', 'json', 'team-list']
+        response = subprocess.check_output(command)
+        for team in json.loads(response)['teams']:
+            if team['name'] == ctx['parameters'][0]:
+                team_id = team['id']
+                break
+        command = base_command + [ctx['action']] + ['--name', ctx['parameters'][0], '--password', ctx['parameters'][1], '--team_id', team_id ]
+    elif 'update' in ctx['action']:
+        command = base_command + ['--format', 'json', 'user-list']
+        response = subprocess.check_output(command)
+        for user in json.loads(response)['users']:
+            if user['name'] == ctx['parameters'][0]:
+                user_id = user['id']
+                user_etag = user['etag']
+                break
+        command = base_command + [ctx['action']] + ['--id', user_id, '--etag', user_etag, '--name', ctx['parameters'][1]]
+    elif 'delete' in ctx['action']:
+        command = base_command + ['--format', 'json', 'user-list']
+        response = subprocess.check_output(command)
+        for user in json.loads(response)['users']:
+            if user['name'] == ctx['parameters'][0]:
+                user_id = user['id']
+                user_etag = user['etag']
+                break
+        command = base_command + [ctx['action']] + ['--id', user_id, '--etag', user_etag]
+
+    return command
+
+
+def team_management(ctx):
+    base_command = ['dcictl', '--dci-login', ctx['login'], '--dci-password', ctx['password'], '--dci-cs-url', ctx['url']]
+
+    if 'list' in ctx['action']:
+        command = base_command + [ctx['action']]
+    elif 'create' in ctx['action']:
+        command = base_command + [ctx['action']] + ['--name', ctx['parameters'][0]]
+    elif 'update' in ctx['action']:
+        command = base_command + ['--format', 'json', 'team-list']
+        response = subprocess.check_output(command)
+        for team in json.loads(response)['teams']:
+            if team['name'] == ctx['parameters'][0]:
+                team_id = team['id']
+                team_etag = team['etag']
+                break
+        command = base_command + [ctx['action']] + ['--id', team_id, '--etag', team_etag, '--name', ctx['parameters'][1]]
+    elif 'delete' in ctx['action']:
+        command = base_command + ['--format', 'json', 'team-list']
+        response = subprocess.check_output(command)
+        for team in json.loads(response)['teams']:
+            if team['name'] == ctx['parameters'][0]:
+                team_id  = team['id']
+                team_etag  = team['etag']
+                break
+        command = base_command + [ctx['action']] + ['--id', team_id, '--etag', team_etag]
+
+    return command
+
+
+def route(ctx):
+    if 'user' in ctx['action']:
+        command = user_management(ctx)
+    elif 'team' in ctx['action']:
+        command = team_management(ctx)
+
+    print subprocess.check_output(command)
+
+
+def main():
+    ctx = parse()
+    route(ctx)
+
+
+if __name__ == "__main__":
+  main()
