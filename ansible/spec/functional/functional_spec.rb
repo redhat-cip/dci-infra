c2e6e51e50fd65f7d0f3de144dd5841f05c16e22
Yanis Guenane <yguenane@redhat.com>
2016-01-26 11:34:58 +0100

    functional testing: provide tests for team management
    Change-Id: I1c51a5409a13fd62c32cc216fed4022212ae806d

--- ansible/spec/functional/functional_spec.rb
+++ ansible/spec/functional/functional_spec.rb
@@ -4,3 +4,41 @@ describe command('dcictl --help') do
   its(:exit_status) { should eq 0 }
 end
 
+# Team management
+#
+describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should contain('admin') }
+  its(:stdout) { should_not match("/|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("./wrapper.sh team-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("./wrapper.sh team-update #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_account_updated']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*Team updated.\s*|/") }
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account_updated']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_account_updated']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("./wrapper.sh team-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account_updated']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*Team deleted.\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("./wrapper.sh team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should_not match("/|\s*#{property['test_account_updated']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
