1ef434b1065159c02f828ffee032386c957a6b3f
Yanis Guenane <yguenane@redhat.com>
2016-01-27 08:10:07 +0100

    functional testing: provide tests for test management
    Change-Id: I2910be8aaf854aec23840491bb352d231a423671

---
+++ ansible/spec/functional/test_management_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+# Test management
+#
+describe command("python wrapper.py test-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should_not match("/|\s*#{property['test_name']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py test-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_name']}") do
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_name']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py test-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_name']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py test-update #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_name']} #{property['test_name_updated']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*Test updated.\s*|/") }
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_name_updated']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py test-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_name_updated']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py test-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_name_updated']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*Test deleted.\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py test-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should_not match("/|\s*#{property['test_name_updated']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
