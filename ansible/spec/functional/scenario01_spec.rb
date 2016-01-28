1a396a22f530a558d8ca27e1de16f6293781e5e2
Yanis Guenane <yguenane@redhat.com>
2016-01-28 11:44:08 +0100

    functional testing: Add scenario01 tests
    Change-Id: I201ff16863323beb1993e13e60519f1589413b80

---
+++ ansible/spec/functional/scenario01_spec.rb
@@ -0,0 +1,95 @@
+require 'spec_helper'
+
+# Scenario01
+#
+# * Create a team
+# * Create a remoteci
+# * Create a user
+# * Run a dummy feeder
+# * Run the helloworld agent
+# * Check the file content
+# * Check the jobstate
+# * Remove the user
+# * Remove the remoteci
+# * Remove the team
+#
+
+describe command("python wrapper.py team-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py remoteci-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_remoteci_name']} #{property['test_password']}") do
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_remoteci_name']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py remoteci-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_remoteci_name']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py user-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_password']}") do
+  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py run-feeder #{property['test_account']} #{property['test_password']} #{property['controlserver_url']} dummy") do
+  its(:stdout) { should contain("created") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py run-agent #{property['test_account']} #{property['test_password']} #{property['controlserver_url']} helloworld #{property['test_remoteci_name']} #{property['test_account']}") do
+  its(:stdout) { should contain("Job scheduled:") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py jobstate-list #{property['test_account']} #{property['test_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should contain("succes") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py file-list #{property['test_account']} #{property['test_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should contain("Helloworld") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py user-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*User deleted.\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should_not match("/|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py remoteci-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_remoteci_name']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*RemoteCI deleted.\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py remoteci-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should_not match("/|\s*#{property['test_remoteci_name']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py team-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+  its(:stdout) { should match("/|\s*message\s*|\s*Team deleted.\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command("python wrapper.py team-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+  its(:stdout) { should_not match("/|\s*#{property['test_account']}\s*|/") }
+  its(:exit_status) { should eq 0 }
+end
