a7d6a40870ac7311f8cefa39d2d18d1d7a330d19
Yanis Guenane <yguenane@redhat.com>
2016-02-18 12:11:51 +0100

    Remove non-installer roles
    dci-doc and reposerver roles were not part of the installer, hence we
    remove them from the code base to host them somewhere else.
    
    Change-Id: I64a06b733228ff8fbc87d7e12dd6273463ecd8f9

--- roles/dci-doc/spec/dci-doc_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe file('/etc/httpd/conf.d/03_dci_doc.conf') do
-  it { should exist }
-end
--- roles/reposerver/spec/reposerver_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe file('/etc/httpd/conf.d/02_dci_repo.conf') do
-  it { should exist }
-end
