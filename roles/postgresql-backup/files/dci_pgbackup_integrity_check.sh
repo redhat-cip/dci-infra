1a05965dc6e780d7a80978b3adb5a6fc21f7e110
Yanis Guenane <yguenane@redhat.com>
2016-04-11 19:10:32 +0200

    postgresql-backup: Initial commit
    This commit aims to bootstrap the backup effort.
    The playbook associated with the postgresql-backup role aims to:
    
      * Install postgresql
      * Install dci-pgbackup
      * Load the dump of the database
      * Verify that the database is accurate through API usage
    
    Change-Id: I6e68c57dca097f0a0b82670e6a00aa98dfcacb80

---
+++ roles/postgresql-backup/files/dci_pgbackup_integrity_check.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+source /etc/dci_pgbackup.conf
+
+function send_mail() {
+    MESSAGE=$1
+    SUBJECT="[ALERT][DCI] Something went wrong with the restore process"
+}
+
+# Retrieve the last record of a given table and verify
+# that the record set match as expected
+for key in {job,user,remoteci,team}; do
+    nbr=$(dcictl --dci-login $DCI_LOGIN --dci-password $DCI_PASSWORD --dci-cs-url $DCI_CS_URL --format json $key-list | jq "[.${key}s][] | length")
+    echo "${key}s,$nbr" >> /tmp/expected_results_api.txt
+done
+
+diff /tmp/expected_results_api.txt $OUTPUT_DIR/expected_results.txt
+if [[ "$?" -ne 0 ]]; then
+    # If backup doesn't match send an email to let team
+    # now something went wrong
+    send_mail "The dump restored is incorrect"
+fi
