b4e3f52e2c2f4d178c99297e37a43c87bcc85774
Gonéri Le Bouder <goneri@redhat.com>
2016-04-20 16:51:10 -0400

    enable pg94 from SCL
    Use PostgreSQL 9.4 inplace of the regular PostgreSQL server. This to be
    able to use the new JSON operators.
    
    Change-Id: I9f11acdd594500172656f7e0b0045042d45bc1da

--- roles/postgresql/spec/postgresql_spec.rb
+++ roles/postgresql/spec/postgresql_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
-describe package('postgresql-server') do
+describe package('rh-postgresql94-postgresql-server') do
   it { should be_installed }
 end
 
-describe service('postgresql') do
+describe service('rh-postgresql94-postgresql') do
   it { should be_enabled }
   it { should be_running }
+end
+
+describe service('postgresql') do
+  it { should be_disabled }
+  it { should be_stopped }
 end
 
 describe port(5432) do
