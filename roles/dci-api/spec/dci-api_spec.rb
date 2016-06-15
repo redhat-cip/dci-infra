bc9c108af01412e3f785681b19549822efccaa50
Yanis Guenane <yguenane@redhat.com>
2016-06-15 15:21:30 +0200

    dci-api: Parametrize the api_filestore_path
    Change-Id: I1e7af527925e9f56c73b22e2fa58c01d6e806302

--- roles/dci-api/spec/dci-api_spec.rb
+++ roles/dci-api/spec/dci-api_spec.rb
@@ -7,3 +7,13 @@ end
 describe file('/etc/httpd/conf.d/dci_api.conf') do
   it { should exist }
 end
+
+describe file("#{property['api_filestore_path']") do
+  it { should be_directory }
+end
+
+describe file("#{property['api_filestore_path']") do
+  it { should be_owned_by 'apache' }
+  it { should be_grouped_into 'apache' }
+  it { should be_writable.by('owner') }
+end
