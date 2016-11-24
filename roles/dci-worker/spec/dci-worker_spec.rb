e0165a9cee5e02e859fc98435b883a863183dd51
Cedric LECOMTE <clecomte@redhat.com>
2016-11-24 15:25:02 -0500

    Add the possibility to deploy a worker
    Change-Id: Ied28c99e1ea637a5a427cb0e6f6deab70b864dd0

---
+++ roles/dci-worker/spec/dci-worker_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe file('/etc/supervisord.d/dci-worker.ini') do
+  it { should exist }
+end
+
+describe package('supervisor') do
+  it { should be_installed }
+end
+
+describe package('postfix') do
+  it { should be_installed }
+end
+
+describe service('postfix') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('supervisord') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(5557) do
+  it { should be_listening }
+end
