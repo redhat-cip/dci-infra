ddf63c1040934a02274b69dce8de49c5e6c75278
Yanis Guenane <yguenane@redhat.com>
2017-05-12 08:49:34 +0200

    user_management: Temporary remove functional tests
    Change-Id: Id290d688b512b475f693048c91f421033925c238

--- spec/functional/scenario01_spec.rb
+++ spec/functional/scenario01_spec.rb
@@ -34,15 +34,15 @@ describe command("python wrapper.py remoteci-list #{property['admin_account']} #
   its(:exit_status) { should eq 0 }
 end
 
-describe command("python wrapper.py user-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_password']}") do
-  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
-  its(:exit_status) { should eq 0 }
-end
+#describe command("python wrapper.py user-create #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']} #{property['test_password']}") do
+#  its(:stdout) { should match("/|\s*name\s*|\s*#{property['test_account']}\s*|/") }
+#  its(:exit_status) { should eq 0 }
+#end
 
-describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
-  its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
-  its(:exit_status) { should eq 0 }
-end
+#describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
+#  its(:stdout) { should match("/|\s*#{property['test_account']}\s*|/") }
+#  its(:exit_status) { should eq 0 }
+#end
 
 #describe command("python wrapper.py run-feeder #{property['test_account']} #{property['test_password']} #{property['controlserver_url']} github https://github.com/redhat-cip/python-dciclient") do
 #  its(:stdout) { should contain("created") }
@@ -74,10 +74,10 @@ end
 #  its(:exit_status) { should eq 0 }
 #end
 
-describe command("python wrapper.py user-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
-  its(:stdout) { should match("/|\s*message\s*|\s*User deleted.\s*|/") }
-  its(:exit_status) { should eq 0 }
-end
+#describe command("python wrapper.py user-delete #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']} #{property['test_account']}") do
+#  its(:stdout) { should match("/|\s*message\s*|\s*User deleted.\s*|/") }
+#  its(:exit_status) { should eq 0 }
+#end
 
 describe command("python wrapper.py user-list #{property['admin_account']} #{property['admin_password']} #{property['controlserver_url']}") do
   its(:stdout) { should_not match("/|\s*#{property['test_account']}\s*|/") }
