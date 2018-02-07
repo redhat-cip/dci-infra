293855595a9a500cf496dcb4a34181609a3d9f18
Yanis Guenane <yguenane@redhat.com>
2018-02-07 10:14:07 +0100

    Add DLRN info in template
    Change-Id: I87464ffecb8da857f92a9018ff7688a073d8def3

--- roles/dci-worker/templates/dci_worker.sh
+++ roles/dci-worker/templates/dci_worker.sh
@@ -1,5 +1,11 @@
 DCI_EMAIL_ACCOUNT='{{ dci_worker_email_account }}'
 DCI_EMAIL_PASSWORD='{{ dci_worker_email_password }}'
 
+DCI_DLRN_LOGIN='{{ dci_worker_dlrn_login }}'
+DCI_DLRN_PASSWORD='{{ dci_worker_dlrn_password }}'
+
 export DCI_EMAIL_ACCOUNT
 export DCI_EMAIL_PASSWORD
+
+export DCI_DLRN_LOGIN
+export DCI_DLRN_PASSWORD
