d78b528a36cd9547ba2087e76369e8ffc43729c6
Guillaume Vincent <gvincent@redhat.com>
2018-12-03 12:12:05 +0100

    Add DCI_EMAIL_SERVER in env variable
    Change-Id: I0e76aeae0b237622f62363d1b0e354285d563045
    Depends-On: I89631c16f49b43f336395d0632b80722a099b91a

--- roles/dci-worker/templates/dci_worker.sh
+++ roles/dci-worker/templates/dci_worker.sh
@@ -1,11 +1,13 @@
+DCI_EMAIL_SERVER='{{ dci_worker_email_server }}'
 DCI_EMAIL_ACCOUNT='{{ dci_worker_email_account }}'
 DCI_EMAIL_PASSWORD='{{ dci_worker_email_password }}'
 
-DCI_DLRN_LOGIN='{{ dci_worker_dlrn_login }}'
-DCI_DLRN_PASSWORD='{{ dci_worker_dlrn_password }}'
-
+export DCI_EMAIL_SERVER
 export DCI_EMAIL_ACCOUNT
 export DCI_EMAIL_PASSWORD
+
+DCI_DLRN_LOGIN='{{ dci_worker_dlrn_login }}'
+DCI_DLRN_PASSWORD='{{ dci_worker_dlrn_password }}'
 
 export DCI_DLRN_LOGIN
 export DCI_DLRN_PASSWORD
