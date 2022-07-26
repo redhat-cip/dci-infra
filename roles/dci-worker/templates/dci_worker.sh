1e0e54038695c907d369c347fabd9724c23fed1f
François Charlier <francois.charlier@redhat.com>
2022-07-26 17:57:02 +0200

    Add some prometheus exporters roles + rework EPEL
    - prometheus-node-exporter
    - prometheus-apache-exporter + update httpd role with it
    - mtail + update httpd role with it
    
    - change how EPEL is managed
    - fix RDO repo conf for RHEL8
    
    Change-Id: I956166be1ded8382cd2a41b25b4874b3a40281e4

--- roles/dci-worker/templates/dci_worker.sh
+++ roles/dci-worker/templates/dci_worker.sh
@@ -1,10 +1,10 @@
 DCI_EMAIL_SERVER='{{ dci_worker_email_server }}'
 DCI_EMAIL_ACCOUNT='{{ dci_worker_email_account }}'
-DCI_EMAIL_PASSWORD='{{ dci_worker_email_password }}'
+{% if dci_worker_email_password|default('') %}DCI_EMAIL_PASSWORD='{{ dci_worker_email_password }}'{% endif %}
 
 export DCI_EMAIL_SERVER
 export DCI_EMAIL_ACCOUNT
-export DCI_EMAIL_PASSWORD
+{% if dci_worker_email_password|default('') %}export DCI_EMAIL_PASSWORD{% endif %}
 
 DCI_DLRN_LOGIN='{{ dci_worker_dlrn_login }}'
 DCI_DLRN_PASSWORD='{{ dci_worker_dlrn_password }}'
